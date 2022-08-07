9e1fe7d34fe3fca044ea9236636044fa0adeb86a
Admin <hanhngao@gmail.com>
2022-08-08 00:39:58 +0700

    'add'

--- [Bài tập] Sử dụng axios để lấy dữ liệu từ API 2/app.js
+++ [Bài tập] Sử dụng axios để lấy dữ liệu từ API 2/app.js
@@ -1,1 +1,11 @@
-axios = require('axios')
+const axios = require("axios")
+
+
+async function getAPI(){
+    let data = await axios.get('https://jsonplaceholder.typicode.com/users')
+    return data.data
+}
+
+getAPI().then((result)=>{
+    console.log(result)
+})
